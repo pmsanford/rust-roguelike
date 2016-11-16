56c0e7e3ad9ef52ae4e6f8348b73acf5be9b7840
Paul Sanford <me@paulsanford.net>
2016-11-16 14:37:35 -0500

    FOV

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate rand;
 
 use tcod::console::*;
 use tcod::colors::{self, Color};
+use tcod::map::{Map as FovMap, FovAlgorithm};
 use rand::Rng;
 
 // actual size of the window
@@ -16,11 +17,19 @@ const MAP_HEIGHT: i32 = 45;
 const LIMIT_FPS: i32 = 20;  // 20 frames-per-second maximum
 
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_LIGHT_WALL: Color = Color { r: 130, g: 110, b: 50 };
 const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+const COLOR_LIGHT_GROUND: Color = Color { r: 200, g: 180, b: 50 };
+
+const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;
+const FOV_LIGHT_WALLS: bool = true;
+const TORCH_RADIUS: i32 = 10;
 
 const ROOM_MAX_SIZE: i32 = 10;
 const ROOM_MIN_SIZE: i32 = 6;
 const MAX_ROOMS: i32 = 30;
+
+
 
 type Map = Vec<Vec<Tile>>;
 
@@ -150,26 +159,36 @@ fn make_map() -> (Map, (i32, i32)) {
     (map, starting_position)
 }
 
-fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
-    // go through all tiles, and set their background color
-    for y in 0..MAP_HEIGHT {
-        for x in 0..MAP_WIDTH {
-            let wall = map[x as usize][y as usize].block_sight;
-            if wall {
-                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-            } else {
-                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map, fov_map: &mut FovMap, fov_recompute: bool) {
+    if fov_recompute {
+        let player = &objects[0];
+        fov_map.compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO);
+
+        // go through all tiles, and set their background color
+        for y in 0..MAP_HEIGHT {
+            for x in 0..MAP_WIDTH {
+                let visible = fov_map.is_in_fov(x, y);
+                let wall = map[x as usize][y as usize].block_sight;
+                let color = match (visible, wall) {
+                    (false, true) => COLOR_DARK_WALL,
+                    (false, false) => COLOR_DARK_GROUND,
+                    (true, true) => COLOR_LIGHT_WALL,
+                    (true, false) => COLOR_LIGHT_GROUND,
+                };
+                con.set_char_background(x, y, color, BackgroundFlag::Set);
             }
         }
-    }
 
-    // draw all objects in the list
-    for object in objects {
-        object.draw(con);
-    }
+        // draw all objects in the list
+        for object in objects {
+            if fov_map.is_in_fov(object.x, object.y) {
+                object.draw(con);
+            }
+        }
 
-    // blit the contents of "con" to the root console
-    blit(con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), root, (0, 0), 1.0, 1.0);
+        // blit the contents of "con" to the root console
+        blit(con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), root, (0, 0), 1.0, 1.0);
+    }
 }
 
 fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
@@ -241,9 +260,22 @@ fn main() {
     let player = Object::new(player_x, player_y, '@', colors::WHITE);
     let mut objects = [player, npc];
 
+    let mut fov_map = FovMap::new(MAP_WIDTH, MAP_HEIGHT);
+
+    for y in 0 .. MAP_HEIGHT {
+        for x in 0 .. MAP_WIDTH {
+            fov_map.set(x, y, 
+                !map[x as usize][y as usize].block_sight,
+                !map[x as usize][y as usize].blocked);
+        }
+    }
+
+    let mut previous_player_position = (-1, 1);
+
     while !root.window_closed() {
         // render the screen
-        render_all(&mut root, &mut con, &objects, &map);
+        let fov_recompute = previous_player_position != (objects[0].x, objects[0].y);
+        render_all(&mut root, &mut con, &objects, &map, &mut fov_map, fov_recompute);
 
         root.flush();
 
@@ -254,6 +286,7 @@ fn main() {
 
         // handle keys and exit game if needed
         let player = &mut objects[0];
+        previous_player_position =  (player.x, player.y);
         let exit = handle_keys(&mut root, player, &map);
         if exit {
             break
