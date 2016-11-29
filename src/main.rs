41c3bc38eb98812c86bb39cbe54686ff8e19e139
Paul Sanford <me@paulsanford.net>
2016-11-29 16:38:25 -0500

    Move choices out of loop

--- src/main.rs
+++ src/main.rs
@@ -604,15 +604,15 @@ fn place_objects(room: &Rect, map: &Map, objects: &mut Vec<Object>) {
 
     let num_monsters = rand::thread_rng().gen_range(0, MAX_ROOM_MONSTERS + 1);
 
+    let monster_chances = &mut [
+        Weighted { weight: 80, item: "orc" },
+        Weighted { weight: 20, item: "troll" },
+    ];
+    let monster_choice = WeightedChoice::new(monster_chances);
+
     for _ in 0 .. num_monsters {
         let x = rand::thread_rng().gen_range(room.x1 + 1, room.x2);
         let y = rand::thread_rng().gen_range(room.y1 + 1, room.y2);
-
-        let monster_chances = &mut [
-            Weighted { weight: 80, item: "orc" },
-            Weighted { weight: 20, item: "troll" },
-        ];
-        let monster_choice = WeightedChoice::new(monster_chances);
 
         if !is_blocked(x, y, map, objects) {
             let mut monster = match monster_choice.ind_sample(&mut rand::thread_rng()) {
